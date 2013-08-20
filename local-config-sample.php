e9d7fa248f1b2daef0eea615a8b62fb87ead3a37
Ferenc Grecu <mivor20@gmail.com>
2013-08-20 08:53:44 +0300

    Change DB_NAME to posticum_host in local-config.php

---
+++ local-config-sample.php
@@ -0,0 +1,26 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'posticum_host' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '127.0.0.1' ); // Probably 'localhost'
+
+
+// ===========
+// Hide errors
+// ===========
+ini_set( 'display_errors', 1 );
+define( 'WP_DEBUG_DISPLAY', true );
+
+// =================================================================
+// Debug mode
+// Debugging? Enable these. Can also enable them in local-config.php
+// =================================================================
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
